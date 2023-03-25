7a2a74c1b80919b54e7f30950184174b23da43b5
bohrapankajs <121491891+bohrapankajs@users.noreply.github.com>
2023-03-25 11:10:32 +0000

    b

---
+++ special.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Name of the script is : $0"     # Gives the name of the script you're running  
+
+echo first value is $1
+echo second value is $2 
+echo third value is $3
+echo $*
+echo $$
+echo $#
+echo $@
