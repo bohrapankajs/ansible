306b9dac186e2643ee5ddb2f972cc1bdcb88baf4
bohrapankajs <121491891+bohrapankajs@users.noreply.github.com>
2023-03-25 10:17:18 +0000

    2

--- color.sh
+++ color.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo -e "\e[34 I am new COlor\e[0m"
-echo -e "\e[36 I am another color\e[0m"
-echo -e "\e[35 Which color I am\e[0m"
+echo -e "\e[34m I am new COlor\e[0m"
+echo -e "\e[36m I am another color\e[0m"
+echo -e "\e[35m Which color I am\e[0m"
 echo -e "\e[32m I am printing Green Color \e[0m"
 echo -e "\e[31m I am printing Red Color \e[0m"
 echo -e "\e[33m I am printing Yellow Color \e[0m"
