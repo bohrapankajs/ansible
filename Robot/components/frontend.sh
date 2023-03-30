174f298e2f624a50f20d17be461f635caca0fc74
Pankajbohra <bohrapankajs@gmail.com>
2023-03-30 15:59:12 +0530

    yes

--- Robot/components/frontend.sh
+++ Robot/components/frontend.sh
@@ -3,6 +3,7 @@ set -e
 
 COMPONENTS=frontend
 USERID=$(id -u)
+LOGFILE=/tmp/$COMPONENTS.log
 
 
 if [ $USERID -ne 0 ] ; then
@@ -20,29 +21,29 @@ stat()
 }
 
 echo -n "Installing nginx:"
-yum install nginx -y &>> /tmp/$COMPONENTS.log 
+yum install nginx -y &>> $LOGFILE
 stat $?
 
 echo -n "Downloading Service:"
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> /tmp/$COMPONENTS.log
+curl -s -L -o /tmp/$COMPONENTS.zip "https://github.com/stans-robot-project/$COMPONENTS/archive/main.zip" &>> $LOGFILE
 stat $?
 
 echo -n "Performing Cleanup:"
-rm -rf  /usr/share/nginx/html/* &>> /tmp/$COMPONENTS.log 
+rm -rf  /usr/share/nginx/html/* &>> $LOGFILE 
 stat $?
 
 cd /usr/share/nginx/html
 echo -n "Unzip the componenets:"
-unzip /tmp/frontend.zip  &>> /tmp/$COMPONENTS.log
+unzip /tmp/$COMPONENTS.zip  &>> $LOGFILE
 stat $?
-mv frontend-main/* . 
+mv $COMPONENTS-main/* . 
 mv static/* .
-rm -rf frontend-master README.md
+rm -rf $COMPONENTS-master README.md
 echo -n "Configuring the Proxyfile:"
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "Enabling Nginx:"
-systemctl enable nginx &>> /tmp/$COMPONENTS.log
-systemctl start nginx  &>> /tmp/$COMPONENTS.log
+systemctl enable nginx &>> $LOGFILE
+systemctl start nginx  &>> $LOGFILE
 stat $?
