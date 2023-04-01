9e67a35a6a54107b4fd7d0439217b2223dd0019e
Pankajbohra <bohrapankajs@gmail.com>
2023-04-01 17:40:00 +0530

    yes

--- Robot/components/common.sh
+++ Robot/components/common.sh
@@ -15,3 +15,86 @@ stat()
         echo -e "\e[31m Faillure\e[0m"
     fi
 }
+
+NODEJS() {
+
+echo -n "Configuring Node JS :"
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+stat $?
+
+echo -n "Installing Node JS :"
+yum install nodejs -y &>> $LOGFILE
+stat $?
+
+
+#calling User creat to add user
+
+CREAT_USER
+
+
+# Download and exract 
+DONWLOAD_AND_EXTRACT
+
+# performing 
+NPM_INSTALL
+
+#Confuring Services 
+CONFIGURE_SERVICE
+
+}
+
+CREAT_USER() {
+
+
+ id $APPUSER  &>> $LOGFILE
+    if [ $? -ne 0 ]; then
+        echo -n " New User Add:"
+        useradd $APPUSER
+        stat $?
+    fi
+}
+
+DONWLOAD_AND_EXTRACT() {
+
+echo -n "Downloading the $COMPONENTS:"
+curl -s -L -o /tmp/$COMPONENTS.zip "https://github.com/stans-robot-project/$COMPONENTS/archive/main.zip" &>> $LOGFILE 
+stat $?
+
+echo -n "Cleaning /up:"
+rm -rf /home/$APPUSER/$COMPONENTS
+cd /home/$APPUSER/
+echo -n "Unzipping $COMPONENTS:"
+unzip -o /tmp/$COMPONENTS.zip &>> $LOGFILE && mv $COMPONENTS-main $COMPONENTS &>> $LOGFILE
+stat $?
+
+echo -n "Changing permission to $APPUSER :"
+chown  $APPUSER:$APPUSER /home/$APPUSER/$COMPONENTS && chmod -R 775 /home/$APPUSER/$COMPONENTS
+stat $?
+
+}
+
+NPM_INSTALL() {
+echo -n " Installing $COMPONENTS Dependenscies:"
+cd $COMPONENTS
+npm install &>> $LOGFILE 
+stat $?
+
+}
+
+CONFIGURE_SERVICE() {
+
+echo -n " Configuring $COMPONENTS service"
+sed -i -e's/MONGO_DNSNAME/172.31.83.219/' -e 's/MONGO_ENDPOINT/172.31.83.219/' -e 's/CATALOGUE_ENDPOINT/172.31.81.242/' -e 's/REDIS_ENDPOINT/172.31.53.189/' /home/$APPUSER/$COMPONENTS/systemd.service
+mv /home/$APPUSER/$COMPONENTS/systemd.service /etc/systemd/system/$COMPONENTS.service
+stat $?
+
+
+echo -n "Starting Component Services :"
+
+systemctl daemon-reload
+systemctl start $COMPONENTS
+systemctl enable $COMPONENTS &>> $LOGFILE 
+stat $?
+
+
+}
--- Robot/components/user.sh
+++ Robot/components/user.sh
@@ -1,58 +1,61 @@
 #!/bin/bash
 
+
+COMPONENTS=user
 source components/common.sh
-COMPONENT=user
 APPUSER=roboshop
-LOGFILE=/tmp/$COMPONENT.log
-
-echo -n "Configuring Node JS :"
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
-stat $?
-
-echo -n "Installing Node JS :"
-yum install nodejs -y &>> $LOGFILE
-stat $?
-
-id $APPUSER &>> $LOGFILE
-    if [ $? -ne 0 ]; then 
-        echo -n "Creating app user :"
-        useradd roboshop
-        stat $?
-    fi
-
-echo -n "Downloading $COMPONENT :"
-curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOGFILE
-stat $?
-
-echo -n "moving files :"
-cd /home/$APPUSER/
-unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
-stat $?
-
-echo -n "Performing cleanup :"
-rm -rf $COMPONENT
-mv $COMPONENT-main $COMPONENT
-stat $?
-
-echo -n "Installing Dependancies:"
-cd $COMPONENT
-npm install &>> $LOGFILE
-stat $?
-
-echo -n "Changing Permissions to  $APPUSER"
-chown $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT && chmod -R 775 /home/$APPUSER/$COMPONENT
-stat $?
-
-echo -n "configuring $COMPONENT service"
-sed -i -e 's/MONGO_ENDPOINT/172.31.83.219/' -e 's/REDIS_ENDPOINT/172.31.53.189/' /home/$APPUSER/$COMPONENT/systemd.service
-mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
-stat $?
+LOGFILE=/tmp/$COMPONENTS.log
+NODEJS
 
 
-echo -n "Starting $COMPONENT service"
-systemctl daemon-reload
-systemctl start $COMPONENT
-systemctl enable $COMPONENT &>> $LOGFILE
-stat $?
+# echo -n "Configuring Node JS :"
+# curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
+# stat $?
 
-echo -n -e "\e[32m___________ $COMPONENT installation completed______________\e[0m"
+# echo -n "Installing Node JS :"
+# yum install nodejs -y &>> $LOGFILE
+# stat $?
+
+# id $APPUSER &>> $LOGFILE
+#     if [ $? -ne 0 ]; then 
+#         echo -n "Creating app user :"
+#         useradd roboshop
+#         stat $?
+#     fi
+
+# echo -n "Downloading $COMPONENTS :"
+# curl -s -L -o /tmp/$COMPONENTS.zip "https://github.com/stans-robot-project/$COMPONENTS/archive/main.zip" &>> $LOGFILE
+# stat $?
+
+# echo -n "moving files :"
+# cd /home/$APPUSER/
+# unzip -o /tmp/$COMPONENTS.zip &>> $LOGFILE
+# stat $?
+
+# echo -n "Performing cleanup :"
+# rm -rf $COMPONENTS
+# mv $COMPONENTS-main $COMPONENTS
+# stat $?
+
+# echo -n "Installing Dependancies:"
+# cd $COMPONENTS
+# npm install &>> $LOGFILE
+# stat $?
+
+# echo -n "Changing Permissions to  $APPUSER"
+# chown $APPUSER:$APPUSER /home/$APPUSER/$COMPONENTS && chmod -R 775 /home/$APPUSER/$COMPONENTS
+# stat $?
+
+# echo -n "configuring $COMPONENTS service"
+# sed -i -e 's/MONGO_ENDPOINT/172.31.83.219/' -e 's/REDIS_ENDPOINT/172.31.53.189/' /home/$APPUSER/$COMPONENTS/systemd.service
+# mv /home/$APPUSER/$COMPONENTS/systemd.service /etc/systemd/system/$COMPONENTS.service
+# stat $?
+
+
+# echo -n "Starting $COMPONENTS service"
+# systemctl daemon-reload
+# systemctl start $COMPONENTS
+# systemctl enable $COMPONENTS &>> $LOGFILE
+# stat $?
+
+echo -n -e "\e[32m___________ $COMPONENTS installation completed______________\e[0m"
