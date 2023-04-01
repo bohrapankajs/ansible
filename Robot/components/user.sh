1939bf6b2d34ae0912b62cee52c9beb8e0792326
Pankajbohra <bohrapankajs@gmail.com>
2023-04-01 16:21:58 +0530

    yes

--- Robot/components/user.sh
+++ Robot/components/user.sh
@@ -1,65 +1,58 @@
 #!/bin/bash
 
 source components/common.sh
-COMPONENTS=user
+COMPONENT=user
 APPUSER=roboshop
-LOGFILE=/tmp/$COMPONENTS.log
+LOGFILE=/tmp/$COMPONENT.log
 
-
-
-echo -n " Downloading File:"
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash - &>> $LOGFILE 
-stat $?
-echo -n " Installing Nodejs:"
-yum install nodejs -y  &>> $LOGFILE 
+echo -n "Configuring Node JS :"
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -  &>> $LOGFILE
 stat $?
 
-id $APPUSER  &>> $LOGFILE
-    
-    
-    if [ $? -ne 0 ]; then
-        echo -n " New User Add:"
-        useradd $APPUSER
+echo -n "Installing Node JS :"
+yum install nodejs -y &>> $LOGFILE
+stat $?
+
+id $APPUSER &>> $LOGFILE
+    if [ $? -ne 0 ]; then 
+        echo -n "Creating app user :"
+        useradd roboshop
         stat $?
     fi
 
-
-
-echo -n "Downloading the $COMPONENTS:"
-curl -s -L -o /tmp/$COMPONENTS.zip "https://github.com/stans-robot-project/$COMPONENTS/archive/main.zip" &>> $LOGFILE 
+echo -n "Downloading $COMPONENT :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip" &>> $LOGFILE
 stat $?
 
+echo -n "moving files :"
 cd /home/$APPUSER/
-echo -n "Unzipping $COMPONENTS:"
-unzip -o /tmp/$COMPONENTS.zip &>> $LOGFILE 
+unzip -o /tmp/$COMPONENT.zip &>> $LOGFILE
+stat $?
+
+echo -n "Performing cleanup :"
+rm -rf $COMPONENT
+mv $COMPONENT-main $COMPONENT
+stat $?
+
+echo -n "Installing Dependancies:"
+cd $COMPONENT
+npm install &>> $LOGFILE
+stat $?
+
+echo -n "Changing Permissions to  $APPUSER"
+chown $APPUSER:$APPUSER /home/$APPUSER/$COMPONENT && chmod -R 775 /home/$APPUSER/$COMPONENT
+stat $?
+
+echo -n "configuring $COMPONENT service"
+sed -i -e 's/MONGO_ENDPOINT/172.31.83.219/' -e 's/REDIS_ENDPOINT/172.31.53.189/' /home/$APPUSER/$COMPONENT/systemd.service
+mv /home/$APPUSER/$COMPONENT/systemd.service /etc/systemd/system/$COMPONENT.service
 stat $?
 
 
-echo -n "Cleaning up:"
-rm -rf $COMPONENTS
-mv $COMPONENTS-main $COMPONENTS
-
-stat $?
-
-echo -n "Installing dependencies :"
-cd $COMPONENTS
-npm install &>> $LOGFILE 
-stat $?
-
-
-echo -n "Changing permission to $APPUSER :"
-chown  $APPUSER:$APPUSER /home/$APPUSER/$COMPONENTS && chmod -R 775 /home/$APPUSER/$COMPONENTS
-stat $?
-
-echo -n " Configuring $COMPONENTS service"
-sed -i -e's/MONGO_ENDPOINT/172.31.83.219/' -e 's/REDIS_ENDPOINT/172.31.93.27/' /home/$APPUSER/$COMPONENTS/systemd.service
-mv /home/$APPUSER/$COMPONENTS/systemd.service /etc/systemd/system/$COMPONENTS.service
-stat $?
-
-
-echo -n "Starting Component Services :"
-
+echo -n "Starting $COMPONENT service"
 systemctl daemon-reload
-systemctl start $COMPONENTS
-systemctl enable $COMPONENTS &>> $LOGFILE 
+systemctl start $COMPONENT
+systemctl enable $COMPONENT &>> $LOGFILE
 stat $?
+
+echo -n -e "\e[32m___________ $COMPONENT installation completed______________\e[0m"
