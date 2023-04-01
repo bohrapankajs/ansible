f2ae9e6e02302467477f56a3f6c31c21f873cd70
Pankajbohra <bohrapankajs@gmail.com>
2023-04-01 13:14:05 +0530

    yes

--- Robot/components/redis.sh
+++ Robot/components/redis.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-echo " I am Redis"
+set -e
+source components/common.sh
+COMPONENTS=redis
+APPUSER=roboshop
+
+echo -n "configuring $COMPONENTS report:"
+curl -L"https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo" -o /etc/yum.repos.d/redis.repo &>> $LOGFILE 
+stat $?
+
+echo -n "Installing $COMPONENTS:"
+yum install redis-6.2.7 -y &>> $LOGFILE
+stat $?
+
+echo - "Whitelisting $COMPONENTS to other:"
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/redis.conf
+stat $?
+
+echo -n "Startng $COMPONENTS service:"
+systemctl daemon-reload
+systemctl enable redis &>> $LOGFILE
+systemctl start redis
+stat $?
