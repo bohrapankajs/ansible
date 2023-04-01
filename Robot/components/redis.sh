c9585d92b2535ba588bf19b3c28a305992d2bd39
Pankajbohra <bohrapankajs@gmail.com>
2023-04-01 14:27:32 +0530

    yes

--- Robot/components/redis.sh
+++ Robot/components/redis.sh
@@ -7,7 +7,7 @@ APPUSER=roboshop
 echo -n "configuring $COMPONENTS report:"
 wget https://download.redis.io/releases/redis-6.2.7.tar.gz &>> $LOGFILE
 tar -xvf  redis-6.2.7.tar.gz &>> $LOGFILE
-cd redis-6.2.7 
+cd /home/centos/Shell_script/Robot/redis-6.2.7/ 
 stat $?
 
 echo -n "Installing $COMPONENTS:"
@@ -15,7 +15,7 @@ yum install redis-* -y &>> $LOGFILE
 stat $?
 
 echo - "Whitelisting $COMPONENTS to other:"
-sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/redis.conf
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/redis.conf && -e 's/127.0.0.0/0.0.0.0/' /etc/redis/redis.conf &>> $LOGFILE
 stat $?
 
 echo -n "Startng $COMPONENTS service:"
