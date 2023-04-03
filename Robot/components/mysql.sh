48045048b10c65876040087fefcc207b16d0ceb9
Pankajbohra <bohrapankajs@gmail.com>
2023-04-03 14:48:46 +0530

    yes

--- Robot/components/mysql.sh
+++ Robot/components/mysql.sh
@@ -3,12 +3,14 @@ COMPONENTS=mysql
 source components/common.sh
 LOGFILE=/tmp/$COMPONENTS.log
 
-echo -n "COnfiguration $COMPONENTS repo:"
+read -p 'Enter Mysql password you wish to configure:' MYSQL_PWD
+
+
+echo -n "Configuration $COMPONENTS repo:"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> $LOGFILE
 stat $?
 
 echo -n "Installing $COMPONENTS :"
-yum remove mariadb-libs -y &>> $LOGFILE
 yum install mysql-community-server -y &>> $LOGFILE
 stat $?
 
@@ -21,17 +23,17 @@ echo -n "Changing the default Password:"
 DEF_ROOT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk -F ' ' '{print $NF}')
 
 
-echo show database | mysql -uroot -pRoboShop@1 &>> $LOGFILE
+echo show database | mysql -uroot -p${MYSQL_PWD} &>> $LOGFILE
 if [ $? -ne 0 ] ; then
     echo -n "Reset root password:"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEF_ROOT_PASSWORD} &>> $LOGFILE
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PWD}';" | mysql --connect-expired-password -uroot -p${DEF_ROOT_PASSWORD} &>> $LOGFILE
     stat $?
 fi
 
-echo show plugins | mysql -uroot -pRoboShop@1 | grep validate_password; &>> $LOGFILE
+echo show plugins | mysql -uroot -p${MYSQL_PWD} | grep validate_password; &>> $LOGFILE
 if [ $? -eq 0 ] ; then
     echo -n "Uninstalling password validate plugin:"
-    echo "uninstall plugin validate_password;" | mysql --connect-expired-password -uroot -pRoboShop@1 &>> $LOGFILE
+    echo "uninstall plugin validate_password;" | mysql --connect-expired-password -uroot -p${MYSQL_PWD} &>> $LOGFILE
     stat $?
 fi
 
@@ -39,10 +41,10 @@ fi
 echo -n "downloading the $COMPONENTS schema:"
 cd /tmp
 # curl -s -L -o /tmp/mysql.zip "https://github.com/stans-robot-project/mysql/archive/main.zip"
-unzip -o $COMPONENTS.zip
+unzip -o $COMPONENTS.zip &>> $LOGFILE
 
 echo -n " Injecting the $COMPENENTS schema:"
 cd /tmp/$COMPONENTS-main
-mysql -u root -pRoboShop@1 <shipping.sql
+mysql -u root -p${MYSQL_PWD} <shipping.sql
 
 echo -n -e "\e[32m___________ $COMPONENTS installation completed______________\e[0m"
