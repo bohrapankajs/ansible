d963c018b6faed5617cd9e379b6563b8fd8a4696
Pankajbohra <bohrapankajs@gmail.com>
2023-04-03 17:31:40 +0530

    yes

--- Robot/components/shipping.sh
+++ Robot/components/shipping.sh
@@ -1,2 +1,45 @@
 #!/bin/bash
-echo " I am Shipping"
+set -e
+
+COMPONENTS=shipping
+source components/common.sh
+APPUSER=roboshop
+
+
+echo -n " Install Maven:"
+
+yum install maven -y &>> $LOGFILE
+
+Stat $?
+
+CREAT_USER
+
+DONWLOAD_AND_EXTRACT
+# echo -n " Downloading the repo:"
+# cd /home/$APPUSER
+# curl -s -L -o /tmp/$COMPONENTS.zip "https://github.com/stans-robot-project/$COMPONENTS/archive/main.zip" &>> $LOGFILE
+# stat $?
+
+# echo -n "Unzip the file :"
+# unzip /tmp/$COMPONENTS.zip &>> $LOGFILE
+# mv $COMPONENTS-main $COMPONENTS
+# stat $?
+
+echo " Install Package :"
+cd $COMPENENTS
+mvn clean package &>> $LOGFILE
+stat $?
+
+echo -n "move Component jar file:"
+mv target/$COMPONENTS-1.0.jar $COMPONENTS.jar 
+stat $?
+
+CONFIGURE_SERVICE
+echo -n -e "\e[32m___________ $COMPONENTS installation completed______________\e[0m"
+
+
+
+
+
+
+
