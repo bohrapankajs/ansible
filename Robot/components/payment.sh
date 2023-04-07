4b17463259fe5cae00e37d70b18ab696e5d61562
Pankajbohra <bohrapankajs@gmail.com>
2023-04-07 15:51:08 +0530

    yes

--- Robot/components/payment.sh
+++ Robot/components/payment.sh
@@ -1,2 +1,37 @@
 #!/bin/bash
-echo " I am payment"
+source components/common.sh
+COMPONENTS=payment
+APPUSER=roboshop
+
+PYTHON(){
+
+
+echo -n " installing Python:"
+yum install python36 gcc python3-devel -y &>> $LOGFILE
+stat $?
+
+# Creating User 
+
+CREAT_USER
+
+# Download the repo.
+DONWLOAD_AND_EXTRACT
+
+#Install the dependencies
+PYTHON_DEPENDECIES
+
+
+# COnfuguring Payment Service
+CONFIGURE_SERVICE
+
+}
+
+PYTHON_DEPENDECIES()
+{
+
+cd /home/$APPUSER/$COMPONENTS 
+pip3 install -r requirements.txt &>> $LOGFILE
+
+
+}
+
