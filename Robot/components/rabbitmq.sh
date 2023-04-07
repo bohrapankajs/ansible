87e4282a20cccd7cd9de148b922ff041d2cf39b5
Pankajbohra <bohrapankajs@gmail.com>
2023-04-07 15:32:11 +0530

    yes

--- Robot/components/rabbitmq.sh
+++ Robot/components/rabbitmq.sh
@@ -6,26 +6,26 @@ source components/common.sh
 APPUSER=roboshop
 
 echo -n "Erlang is a dependency which is needed for $COMPONENTS: "
-yum install https://github.com/$COMPONENTS/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y $LOGFILE
+yum install https://github.com/$COMPONENTS/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> $LOGFILE
 stat $?
 
 echo -n "Setup YUM repositories for $COMPONENTS: "
-curl -s https://packagecloud.io/install/repositories/$COMPONENTS/$COMPONENTS-server/script.rpm.sh | sudo bash $LOGFILE
+curl -s https://packagecloud.io/install/repositories/$COMPONENTS/$COMPONENTS-server/script.rpm.sh | sudo bash &>> $LOGFILE
 stat $?
 
 echo -n " Install $COMPONENTS: "
-yum install $COMPONENTS-server -y $LOGFILE
+yum install $COMPONENTS-server -y &>> $LOGFILE
 stat $?
 
 echo -n "Start $COMPONENTS: "
-systemctl enable $COMPONENTS-server $LOGFILE
+systemctl enable $COMPONENTS-server &>> $LOGFILE
 systemctl start $COMPONENTS-server 
 stat $?
 
 echo -n "Creating application user: "
-rabbitmqctl add_user roboshop roboshop123 $LOGFILE
-rabbitmqctl set_user_tags roboshop administrator $LOGFILE
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  $LOGFILE
+rabbitmqctl add_user roboshop roboshop123 &>> $LOGFILE
+rabbitmqctl set_user_tags roboshop administrator &>> $LOGFILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>> $LOGFILE
 stat $?
 
 
