f1d4636b6df880fb616fa6ee53f231c8857aacb8
Pankajbohra <bohrapankajs@gmail.com>
2023-04-08 14:07:10 +0530

    yes

--- Robot/components/Patch.sh
+++ Robot/components/Patch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+source components/common.sh
 for component in catalogue user cart shipping payment; do 
+    echo -n " For $component runs:"
     sed -i -e "/$component/s/localhost/$component.robo.internal/" /tmp/roboshop.conf
     stat $?
 done
