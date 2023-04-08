0135b836160b30df5900e79a0ff0b0f3bad5c082
Pankajbohra <bohrapankajs@gmail.com>
2023-04-08 13:51:50 +0530

    yes

--- Robot/components/Patch.sh
+++ Robot/components/Patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for COMP in catalogue user cart shipping payment ; do 
-sed -e '/$COMP/s/localhost/$COMP.robo.internal/' /tmp/roboshop.conf
+sed -i -e '/$COMP/s/localhost/$COMP.robo.internal/' /tmp/roboshop.conf
 done
