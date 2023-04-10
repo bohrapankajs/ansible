ce6704512a70699626743c67a017cf524a4e3d27
Pankajbohra <bohrapankajs@gmail.com>
2023-04-10 17:59:34 +0530

    yes

--- Robot/create_server.sh
+++ Robot/create_server.sh
@@ -8,7 +8,7 @@ fi
 COMPONENT=$1
 
 
-AMI_ID="$(aws ec2 describe-instances | jq '.Reservations[].Instances[].ImageId' | sed -e 's/"//g')"
+AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=Free-Everyone | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 echo "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SGID"
@@ -16,7 +16,7 @@ echo $COMPONENT
 
 CREATE_SERVER()
 {
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID 
 }
 
 if [ "$1" == "all" ] ; then
