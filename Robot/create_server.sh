83748c9fa8286b624d792e6278ba1a20f08afa11
Pankajbohra <bohrapankajs@gmail.com>
2023-04-20 17:15:42 +0530

    yes

--- Robot/create_server.sh
+++ Robot/create_server.sh
@@ -9,8 +9,8 @@ COMPONENT=$1
 # ENV=$2
 ZONE_ID="Z0892453GPU3MJPYG1MJ"
 
-# AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
-AMI_ID="ami-0fa1ba08307b907ac"
+AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
+# AMI_ID="ami-0fa1ba08307b907ac"
 SGID="$(aws ec2 describe-security-groups   --filters Name=group-name,Values=Free-Everyone | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 # SGID=sg-0d1a2e2c970155a53
 echo "AMI ID Used to launch instance is : $AMI_ID"
@@ -18,9 +18,9 @@ echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
 
 createServer() {
-    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
+    PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT-/" route53.json > /tmp/dns.json 
+    sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/$COMPONENT/" route53.json > /tmp/dns.json 
 
     echo -n "Creating the DNS Record ********"
     aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch file:///tmp/dns.json | jq 
